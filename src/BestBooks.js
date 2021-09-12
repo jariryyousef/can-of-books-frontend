244a50837fbc63711892a83d4c39229efd824a97
jariryyousef <jariryyousef@gmail.com>
2021-09-12 21:39:49 +0300

    finsh

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,30 +1,63 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from 'react-bootstrap/Card';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
-    }
+      books: [],
+    };
   }
 
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
 
+  componentDidMount = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/book`)
+      .then((bookResponse) => {
+        this.setState({ books: bookResponse.data });
+      })
+      .catch((error) => alert(error.message));
+  };
+
   render() {
 
+    // return(<p>test</p>)
+
     /* TODO: render user's books in a Carousel */
+    <div>
+     
+      {this.state.books.length > 0 && (
+        <>
+         
+          {this.state.books.map((book) => {
+            return (
+              <>
+                
+                <Card style={{ width: "18rem" }}>
+                  
+                  <Card.Body>
+                    
+                    <Card.Title>{book.title}</Card.Title>
+                    <Card.Text> {book.description} </Card.Text>
+                    <Card.Text> {book.status} </Card.Text>
+                    <Card.Text> {book.email} </Card.Text> 
 
-    return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+                    {/* <Card.Title>t</Card.Title>
+                    <Card.Text> e </Card.Text>
+                    <Card.Text> s </Card.Text>
+                    <Card.Text> t </Card.Text>  */}
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
-    )
+                    </Card.Body>
+                </Card>
+              </>
+            );
+          })}
+        </>
+      )}
+    </div>;
   }
 }
 
--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,10 +1,17 @@
 import { Component } from 'react'
 
+import Button from 'react-bootstrap/Button'
+
 export default class LoginButton extends Component {
 
   render() {
 
-    /* TODO: Render a button with label 'Log In'. When the button is clicked then show LoginForm instead */
-    return <p>LoginButton coming soon</p>
+    return(
+      <>
+      <Button variant="primary">Log In</Button>
+     
+    </>
+    ) 
   }
 }
+
